141630e1058e27124264649beac7f1460cd584f3
Daniel Josh Uduma <116913814+Danieljosh-uduma@users.noreply.github.com>
2024-11-17 06:54:24 +0000

    Delete week1 directory

--- week1/js/main.js
+++
@@ -1,8 +0,0 @@
-let arrowBack = document.getElementById('arrowback');
-let chatBox = document.getElementById('chatbox');
-let chatList = document.getElementById('chatlist');
-
-arrowBack.onclick = function (){
-    chatBox.style.display = 'none';
-    chatList.style.display = 'block';
-}
